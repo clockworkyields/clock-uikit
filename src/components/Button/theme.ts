5f119940801e4fd9e24feee12cb917cba4e1375e
cockswapDev <cockswapDev@protonmail.com>
2021-05-10 22:14:15 -0400

    0.2.1

--- src/components/Button/theme.ts
+++ src/components/Button/theme.ts
@@ -38,7 +38,7 @@ export const styleVariants = {
   },
   [variants.SUBTLE]: {
     backgroundColor: "textSubtle",
-    color: "white",
+    color: "black",
   },
   [variants.DANGER]: {
     backgroundColor: "failure",
